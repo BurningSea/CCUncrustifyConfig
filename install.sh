f56c3eb1ca58070f462bbadb6b28bfbb6a914c5a
hehai <hehai@bytedance.com>
2016-07-19 20:34:07 +0800

    修改安装脚本
    Change-Id: I6c2bca041e28c7a7d8f6e04b7210d3c03a2b6317

--- install.sh
+++ install.sh
@@ -5,13 +5,17 @@ if [ ! -d "${PLUGIN_LOCATION}" ]; then
 	echo "Plugin not installed, start to download"
 	curl -SL https://raw.githubusercontent.com/benoitsan/BBUncrustifyPlugin-Xcode/master/install.sh | sh
 else
-	echo "Plugin Installed"
+	echo "Plugin installed!"
 fi
 
 CONFIG_URI=https://github.com/BurningSea/CCUncrustifyConfig.git
 CONFIG_DIR="${HOME}/.uncrustify"
-mkdir -p "${CONFIG_DIR}"
-git clone $CONFIG_URI "${CONFIG_DIR}"
+if [ ! -d "${CONFIG_DIR}" ]; then
+	mkdir -p "${CONFIG_DIR}"
+	git clone $CONFIG_URI "${CONFIG_DIR}"
+else
+	echo "You've cloned the config dir!"
+fi
 
-echo "\nConfig successfuly cloned! Plz restart your Xcode and \nSelect Clang for the Formatter in Edit->Format Code->Preferences\nAnd select Custom Style (File) for Clange Style"
+echo "\nConfig successfuly cloned! Plz restart your Xcode \nAnd select Clang for the Formatter in Edit->Format Code->Preferences\nAnd select Custom Style (File) for Clang Style"
 echo "\nFinally copy ~/.uncrustify/.clang-format to your Project! Enjoy it."
